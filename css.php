18dd3712062634a9b61442f5016c7f2ca7cdbaad
naosia <naosia1@gmail.com>
2011-07-06 22:38:25 +0200

    Added licensing

--- css.php
+++ css.php
@@ -1,3 +1,13 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Emil Karlsson and DUke Derpington wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy us a beer in return. Emil Karlsson and Duke Derpington
+ * ----------------------------------------------------------------------------
+ */
+
+
 
 <? /* This file will need to be MIME-identified as 'text/css' on the webserver's side. */
 
