9674d635b8de5816dc967cb985eb266287645a12
naosia <naosia1@gmail.com>
2011-07-07 01:19:34 +0200

    Quick bug fix

--- modules/post_count.php
+++ modules/post_count.php
@@ -1,6 +1,8 @@
 
 <?php
 
+include "../config.php"
+
 $linecount_nick = sprintf("SELECT COUNT(*) FROM %s WHERE name='%s'",
 	mysql_real_escape_string($tablename),
 	mysql_real_escape_string($_GET['name']));
