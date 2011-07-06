b2d00f0f5136eb0e401c6544ddf73c391c49f983
naosia <naosia1@gmail.com>
2011-07-07 01:27:57 +0200

    Quick bug fix

--- modules/post_count.php
+++ modules/post_count.php
@@ -2,15 +2,15 @@
 <?php
 
 include "../config.php"
+if($_GET['name']) {
+    $linecount_nick = sprintf("SELECT COUNT(*) FROM %s WHERE name='%s'",
+	    mysql_real_escape_string($tablename),
+	    mysql_real_escape_string($_GET['name']));
+	    
+    $linecount_nick_last = sprintf("SELECT * FROM %s WHERE name='%s' ORDER BY lineID DESC LIMIT 1",
+        mysql_real_escape_string($tablename),
+        mysql_real_escape_string($_GET['name']));
 
-$linecount_nick = sprintf("SELECT COUNT(*) FROM %s WHERE name='%s'",
-	mysql_real_escape_string($tablename),
-	mysql_real_escape_string($_GET['name']));
-	
-$linecount_nick_last = sprintf("SELECT * FROM %s WHERE name='%s' ORDER BY lineID DESC LIMIT 1",
-    mysql_real_escape_string($tablename),
-    mysql_real_escape_string($_GET['name']));
-
-$linecount_total = mysql_query('SELECT COUNT(*) FROM lines');
-
+    $linecount_total = mysql_query('SELECT COUNT(*) FROM lines');
+}
 ?>
