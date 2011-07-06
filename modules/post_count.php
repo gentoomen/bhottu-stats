3e1620824460ddfb271bccd9d3853844bdebeb6c
naosia <naosia1@gmail.com>
2011-07-06 23:18:07 +0200

    Added basic linecount statistics

--- modules/post_count.php
+++ modules/post_count.php
@@ -8,4 +8,10 @@
  */
 
 
-<? /*stuff*/ ?>
+<?php
+
+$linecount_nick = sprintf("SELECT COUNT(*) FROM %s WHERE name='%s'",
+	mysql_real_escape_string($tablename),
+	mysql_real_escape_string($_GET['name']));
+
+$linecount_total = mysql_query('SELECT COUNT(*) FROM lines');
