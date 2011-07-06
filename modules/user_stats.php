0e90ef8f6040cf5a941b50cb0ed3769b65afce88
Duke Derpington of Chesterfieldshire <poopsmith@lavabit.com>
2011-07-07 01:44:12 +0200

    checking for mysql errors

--- modules/user_stats.php
+++ modules/user_stats.php
@@ -4,12 +4,18 @@
 $top15 = "";
 $resource = mysql_query("SELECT name, points FROM nickscore ORDER BY points DESC LIMIT 15");
 
-$bros = false;
+if (!$resource) {
+    $bros = false;
 
-while($row = mysql_fetch_row($resource)){
-    if (!$bros){$bros = $row};
-    $last_post = mysql_fetch_row(mysql_query(sprintf("SELECT message FROM lines WHERE name=%s ORDER BY lineID ASC LIMIT 1", mysql_real_escape_string($row[0]))))[0];
-    $top15 = $top15 . "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>". $last_post . "</td></tr>\n";
+    while($row = mysql_fetch_row($resource)){
+        if (!$bros){$bros = $row};
+        $last_post = mysql_fetch_row(mysql_query(sprintf("SELECT message FROM lines WHERE name=%s ORDER BY lineID ASC LIMIT 1", mysql_real_escape_string($row[0]))))[0];
+        $top15 = $top15 . "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>". $last_post . "</td></tr>\n";
+    }
+};
+
+if $top15 == "" {
+    $top15 = mysql_error();
 }
 
 ?>
