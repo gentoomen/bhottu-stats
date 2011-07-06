4c182b3a51f8ded63206196888e2f0b5bc264519
naosia <naosia1@gmail.com>
2011-07-07 00:22:00 +0200

    Added some stuff to index.php

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 
 <? include "main.php" ?>
 
 <html>
+    <head>
+        <meta name="description" content="<?php echo('IRC channel stats for $irc_channel @ $irc_network') ?>" />
+        <meta name="keywords" content="<?php echo('IRC,Stats,$irc_channel,$irc_network,bhottu') ?>" />
+        <meta name="author" content="Gentoomen" />
+        <meta http-equiv="content-type" content="text/x-php;charset=UTF-8" />
+    </head>
     <body>
         <center>
             <table>
                 <tr>
-                    <td>Total Post count:</td>
-                    <td>Most talkative bro:</td>
+                    <td>Total line count:</td>
+                    <td>Top 15:</td>
                     <td>Most banned person:</td>
                 </tr>
                 <tr>
-                    <td><? echo($post_count) ?></td>
-                    <td><? echo($bros[0]) ?></td>
+                    <td><? echo($linecount_total) ?></td>
+                    <td><? echo($top15) ?></td>
                     <td>nand</td>
                 </tr>
             </table>
@@ -24,11 +31,16 @@
                 <?= $top15 ?>
             </table>
            
-            <form method="get" action="index.php">
-                <input type="text" name="name" />
-                <input type="submit" value="Look up the line count of a specific bro" />
-            </form>
-            <?php if($_GET['name']){echo('<p>This bro has written $linecount_nick lines.</p>')} ?>
+            <table>
+                <tr>
+                <td class="headline"><b>Look up the line count of a specific bro</b>
+                </tr><tr>
+                <form method="get" action="index.php">
+                    <input type="text" name="name" />
+                    <input type="submit" value="Look up the line count of a specific bro" />
+                </form>
+
+            } ?>
         </center>
     </body>
 </html>
