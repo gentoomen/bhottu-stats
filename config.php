56c0f6c4bc1f7508eefb90512dbc120245d30285
nep <poopsmith@lavabit.com>
2011-08-29 16:34:57 +0200

    "re-wording"

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
-
 <?
 
 //SQL
+//steal this stuff from bhottu's config.py file :P
 $db_server = "localhost"; //include port number here btw
-$db_user = ""; //steal from bhottu's configs :P
+$db_user = "";
 $db_pass = "";
-$db_name = ""; //actual database name
+$db_name = "";
 
 //LAYOUT
-$layout_font_color = "";
-$layout_background_color = "";
+$layout_font_color = "#00ff00";
+$layout_background_color = "#daa520";
 
 //MODULES
-$modules_directory = ""; //Must end in "/"!
-
+$modules_directory = "";
+//Must end in "/"!
 ?>
--- css.php
+++ css.php
@@ -1,8 +1,6 @@
-
 <? /* This file will need to be MIME-identified as 'text/css' on the webserver's side. */
 
 include "config.php";
-
 ?>
 
 body {
@@ -10,6 +8,6 @@ body {
 color: <?= $layout_font_color ?>;
 background-color: <?= $layout_background_color ?>;
 
-<? /* Whatever else... */ ?>
+<? /* Whatever else... */?>
 
 }
